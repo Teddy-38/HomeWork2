3f5fe1099a9b64847e08b2dc8ec6c99134a89ec2
Teddy-38 <Zhdanov_as94@mail.ru>
2024-12-29 20:47:41 +0800

    End

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //write your code here
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        var result = dog + 4;
+        System.out.println(result);
+        var result2 = cat + 4;
+        System.out.println(result2);
+        var result3 = paper + 4;
+        System.out.println(result3);
+        var result4 = dog - 3.5;
+        System.out.println(result4);
+        var result5 = cat - 1.6;
+        System.out.println(result5);
+        var result6 = paper - 7639;
+        System.out.println(result6);
+        var friend = 19;
+        System.out.println(friend);
+        var result7 = friend + 2;
+        System.out.println(result7);
+        var result8 = result7 / 7;
+        System.out.println(result8);
+        var frog = 3.5;
+        System.out.println(frog);
+        var result9 = frog * 10;
+        System.out.println(result9);
+        var result10 = result9 / 3.5;
+        System.out.println(result10);
+        var result11 = result10 + 4;
+        System.out.println(result11);
+        var box1 = 78.2;
+        var box2 = 82.7;
+        var result12 = box1 + box2;
+        System.out.println(result12);
+        var result13 = box2 - box1;
+        System.out.println(result13);
+        var result14 = box2 % box1;
+        System.out.println(result14);
+        var workClock = 640;
+        var peopleClock = 8;
+        var result15 = workClock / peopleClock;
+        System.out.println("Всего работников в компании-" + result15 + "человек");
+        var result16 = result15 + 94;
+        var result17 = workClock / result16;
+        System.out.println("Если в компании работает " +result16+ " человека, то всего " +result17+ " часа работы может быть поделено между сотрудниками");
     }
 }
